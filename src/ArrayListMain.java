0b4136343d3d7e981bf7c5a62401c78d14577e92
Phone Wint Tun <155772186+OutKast317@users.noreply.github.com>
2025-04-29 12:17:15 +0630

    feat: arrayList with numbers

---
+++ src/ArrayListMain.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class ArrayListMain {
+    public static void main(String[] args) {
+      ArrayList<Double> list = new ArrayList<Double>();
+      
+      list.add(6.3);
+      list.add(5.5);
+      list.add(1.7);
+      list.add(2.1);
+      list.add(3.0);
+      
+      System.out.println("Size of array list: " + list.size());//size of ArrayList
+      System.out.println("Original: " + list);
+      
+      //adding an element to a specific Index
+      list.add(1, 4.6);
+      System.out.println("After adding: " + list);
+      
+      //accessing an index
+      System.out.println("Getting an item: " + list.get(0));
+      
+      //looping through ArrayList
+      System.out.println("\nLooping through the array list: ");
+      for (Double i : list){
+        System.out.println(i);
+      }
+      
+      //sorting an array list
+      Collections.sort(list);
+      System.out.println("\nAfter sorting (ascending): " + list);//ascending order
+      
+      Collections.sort(list, Collections.reverseOrder());//descending order
+      System.out.println("After sorting (descending): " + list);
+      
+      //changing an item
+      list.set(0, 6.5);
+      System.out.println("After changing: " + list);
+      
+      //removing an item
+      list.remove(2);//removing by defining an index 
+      list.remove("F");//removing by defining an item
+      System.out.println("After removing: " + list);
+      
+  }
+}
